9e1145b105860e2a19a3ce352a0077e591173dc3
Robyn Moncrief <44477525+12obyn@users.noreply.github.com>
2019-04-17 16:07:32 -0500

    Quit sharing your work
    You guys can help each other, but do not copy directly. Make sure you try to solve the problem and understand what the program is doing. If you are sharing work, make sure those you share it with have tried to solve it first and that after helping them that they understand what the program is doing.
    
    Fix any comments I made within the program.

--- Lab activity 2.py
+++ Lab activity 2.py
@@ -1,13 +1,13 @@
 # Jamaree Jackson
 #4/9/2019
 
-#problem 2 This tells you the day that you returen on a specific day
+#problem 2 This tells you the day that you returen on a specific day # Why is this exactly what Deshawn wrote?
 
 day = int(input("number 0 - 6"))
 
 gone = int(input("How long were you gone?"))
 
-gone % 3
+gone % 3 # What does this line do?
 
-((gone % 3) + day) 
-print(((gone % 3) + day) % 3)
+((gone % 3) + day) # What does this line do?
+print(((gone % 3) + day) % 3) # Why did you choose 3?
